170abe0a8f8a32ae3ef895995e41d972d65e7f04
Artur <arturpeixotoo@gmail.com>
2022-09-10 19:01:23 -0300

    end project

--- src/pages/Completed/index.tsx
+++ src/pages/Completed/index.tsx
@@ -1,16 +1,63 @@
+import { MapPin, CurrencyDollar, Timer } from 'phosphor-react'
 import React from 'react'
-
-import { Container } from './styles'
+import illustration from '../../assets/illustration.svg'
+import { defaultTheme } from '../../styles/themes/default'
+import {
+  AddressDetails,
+  Container,
+  TextConfirmationOrder,
+  SubTitleConfirmationOrder,
+  DescriptionDeliveryInfo,
+} from './styles'
 
 export function Completed() {
   return (
     <Container>
       <div>
-        <text>Uhu! Pedido confirmado</text>
-        <text>Agora é só aguardar que logo o café chegará até você</text>
-        <div></div>
+        <TextConfirmationOrder>Uhu! Pedido confirmado</TextConfirmationOrder>
+        <br />
+        <SubTitleConfirmationOrder>
+          Agora é só aguardar que logo o café chegará até você
+        </SubTitleConfirmationOrder>
+        <AddressDetails>
+          <div>
+            <DescriptionDeliveryInfo backgroundIconColor="purple">
+              <MapPin size={18} weight="fill" color={defaultTheme.background} />
+              <label>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Farrapos - Porto Alegre, RS
+              </label>
+            </DescriptionDeliveryInfo>
+            <DescriptionDeliveryInfo backgroundIconColor="yellowLight">
+              <Timer size={18} weight="fill" color={defaultTheme.background} />
+              <label>
+                Previsão de entrega <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <strong>20 min - 30 min </strong>
+              </label>
+            </DescriptionDeliveryInfo>
+
+            <DescriptionDeliveryInfo backgroundIconColor="yellow">
+              <CurrencyDollar
+                size={18}
+                weight="fill"
+                color={defaultTheme.background}
+              />
+              <label>
+                Pagamento na entrega
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <strong>Cartão de Crédito</strong>
+              </label>
+            </DescriptionDeliveryInfo>
+          </div>
+        </AddressDetails>
       </div>
-      <div></div>
+      <div>
+        <img src={illustration} />
+      </div>
     </Container>
   )
 }
--- src/pages/Completed/styles.ts
+++ src/pages/Completed/styles.ts
@@ -3,4 +3,73 @@ import styled from 'styled-components'
 export const Container = styled.div`
   margin-top: 4.5rem;
   display: flex;
+  img {
+    margin-left: 10rem;
+    margin-top: 5rem;
+  }
+`
+
+export const AddressDetails = styled.div`
+  width: 32.875rem;
+  height: 18rem;
+  margin-top: 2.5rem;
+  box-sizing: border-box;
+  border: 1px solid #8047f8;
+  border-radius: 6px 36px;
+  padding: 40px;
+  div:first-child {
+    margin-top: 0;
+  }
+`
+
+export const TextConfirmationOrder = styled.text`
+  font-family: 'Baloo 2';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 32px;
+
+  color: ${(props) => props.theme['yellow-dark']};
+`
+
+export const SubTitleConfirmationOrder = styled.text`
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+`
+const STATUS_COLORS = {
+  yellow: 'yellow-dark',
+  yellowLight: 'yellow',
+  baseText: 'base-text',
+  purple: 'purple',
+} as const
+
+interface StatusProps {
+  backgroundIconColor: keyof typeof STATUS_COLORS
+}
+
+export const DescriptionDeliveryInfo = styled.div<StatusProps>`
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+
+  label {
+    color: ${(props) => props.theme['base-text']};
+    margin-left: 12px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 130%;
+  }
+
+  svg {
+    border: 6px solid
+      ${(props) => props.theme[STATUS_COLORS[props.backgroundIconColor]]};
+    background-color: ${(props) =>
+      props.theme[STATUS_COLORS[props.backgroundIconColor]]};
+    border-radius: 20px;
+    margin-bottom: -6px;
+  }
+  margin-top: 2rem;
 `
